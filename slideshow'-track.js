ba7b0a977aa05347485c18ba6abfbb184dd3651a
womghei <womghei@connect.hku.hk>
2019-03-06 12:45:40 +0800

    Create slideshow'-track.js

---
+++ slideshow'-track.js
@@ -0,0 +1,6 @@
+jQuery('.slideshow-next').click(function(){
+    utag.link({event_action: 'next slideshow'})
+})
+jQuery('.slideshow-prev').click(function(){
+    utag.link({event_action: 'previous slideshow'})
+})
